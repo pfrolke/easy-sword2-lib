dacc520d6f8f3367b1bd236ebb0c0271f83ec1ae
Paul Boon <paul.boon@dans.knaw.nl>
2017-05-23 09:13:58 +0200

    added atom Links to the ResourcePart and updated the AtomStatement.writeTo to write them

--- src/main/java/org/swordapp/server/ResourcePart.java
+++ src/main/java/org/swordapp/server/ResourcePart.java
@@ -1,12 +1,15 @@
 package org.swordapp.server;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class ResourcePart {
     protected String uri;
     protected String mediaType;
     protected Map<String, String> properties = new HashMap<String, String>();
+    protected List<String> selfLinks = new ArrayList<String>();
 
     public ResourcePart(String uri) {
         this.uri = uri;
@@ -31,4 +34,13 @@ public class ResourcePart {
     public void addProperty(String predicate, String object) {
         this.properties.put(predicate, object);
     }
+
+    public List<String> getSelfLinks() {
+        return selfLinks;
+    }
+
+    public void addSelfLink(String link) {
+        selfLinks.add(link);
+    }
+
 }
